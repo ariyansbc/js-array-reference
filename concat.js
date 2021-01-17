8f2718b173558ee835bcaa9a0a48a62499bc32ad
shahriar jalal <shahjalalsbc@gmail.com>
2021-01-17 19:10:24 +0600

    js concat

---
+++ concat.js
@@ -0,0 +1,17 @@
+//join two arrays
+
+var person1 = ["jalal", 'sms'];
+var person2 = ["shahriar", 'shakhawat'];
+
+var fullname = person1.concat(person2);
+
+console.log(fullname);
+
+//join multiple arrays
+var firstname  = ['shahriar', 'ariayn', 'rubel','rimi'];
+var lastname = ['jalal', 'khan', 'ahmed', 'khatun'];
+var age = [25, 30 , 21 , 17];
+
+var details = firstname.concat(lastname, age);
+
+console.log(details);
